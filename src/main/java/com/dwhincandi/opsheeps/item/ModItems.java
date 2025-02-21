dedf1ba6f4ee950933b20247c11fea68e4686528
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-02-21 19:43:18 +0100

    added rarity

--- src/main/java/com/dwhincandi/opsheeps/item/ModItems.java
+++ src/main/java/com/dwhincandi/opsheeps/item/ModItems.java
@@ -7,16 +7,17 @@ import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 public class ModItems {
 
     public static final Item SUPER_DYE = registerItem("super_dye", new SuperDyeItem(new Item.Settings()));
-    public static final Item SUPER_SHEAR_V1 = registerItem("super_shear_v1", new SuperShearItem(new Item.Settings().maxDamage(-1), 2));
-    public static final Item SUPER_SHEAR_V2 = registerItem("super_shear_v2", new SuperShearItem(new Item.Settings().maxDamage(-1), 5));
-    public static final Item SUPER_SHEAR_V3 = registerItem("super_shear_v3", new SuperShearItem(new Item.Settings().maxDamage(-1), 10));
-    public static final Item SUPER_SHEAR_V4 = registerItem("super_shear_v4", new SuperShearItem(new Item.Settings().maxDamage(-1), 30));
-    public static final Item SUPER_SHEAR_V5 = registerItem("super_shear_v5", new SuperShearItem(new Item.Settings().maxDamage(-1), 100));
-    public static final Item SUPER_SHEAR_V69 = registerItem("super_shear_v69", new SuperShearItem(new Item.Settings().maxDamage(-1), 250));
+    public static final Item SUPER_SHEAR_V1 = registerItem("super_shear_v1", new SuperShearItem(new Item.Settings().maxDamage(-1).rarity(Rarity.EPIC), 2));
+    public static final Item SUPER_SHEAR_V2 = registerItem("super_shear_v2", new SuperShearItem(new Item.Settings().maxDamage(-1).rarity(Rarity.COMMON), 5));
+    public static final Item SUPER_SHEAR_V3 = registerItem("super_shear_v3", new SuperShearItem(new Item.Settings().maxDamage(-1).rarity(Rarity.COMMON), 10));
+    public static final Item SUPER_SHEAR_V4 = registerItem("super_shear_v4", new SuperShearItem(new Item.Settings().maxDamage(-1).rarity(Rarity.UNCOMMON), 30));
+    public static final Item SUPER_SHEAR_V5 = registerItem("super_shear_v5", new SuperShearItem(new Item.Settings().maxDamage(-1).rarity(Rarity.RARE), 100));
+    public static final Item SUPER_SHEAR_V69 = registerItem("super_shear_v69", new SuperShearItem(new Item.Settings().maxDamage(-1).rarity(Rarity.EPIC), 250));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Opsheeps.MOD_ID, name), item);
