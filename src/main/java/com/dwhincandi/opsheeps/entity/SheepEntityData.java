b9fc9c98bbd55218457f57ae06f2dd272f75f1ae
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-02-22 08:17:34 +0100

    added an logger

--- src/main/java/com/dwhincandi/opsheeps/entity/SheepEntityData.java
+++ src/main/java/com/dwhincandi/opsheeps/entity/SheepEntityData.java
@@ -4,7 +4,19 @@ import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class SheepEntityData {
+    public static final Logger LOGGER = LogManager.getLogger(SheepEntityData.class);
+
     public static final TrackedData<Boolean> IS_OP_SHEEP = DataTracker.registerData(SheepEntity.class, TrackedDataHandlerRegistry.BOOLEAN);
+
+    static {
+        try {
+            LOGGER.debug("IS_OP_SHEEP DataTracker für SheepEntity registriert.");
+        } catch (Exception e) {
+            LOGGER.error("Error occurred while registering IS_OP_SHEEP DataTracker", e);
+        }
+    }
 }
